8ba821c9b6f3e1f63e8d60480686ef88e6df9880
Guelito1 <mflorescacravens@gmail.com>
2019-06-03 08:40:21 -0700

    commit for hangman

---
+++ js/script.js
@@ -0,0 +1,145 @@
+
+
+function go() {
+    alert("Hello! Welcome to Hangman! Please guess a letter in the fields below. Hint: The word isn't very sweet")
+};
+
+go();
+
+// start with variable declarations
+
+var head = document.queryselector("head");
+var rArm = document.queryselector("rArm");
+var lArm = document.queryselector("lArm");
+var torso = document.queryselector("torso");
+var rLeg = document.queryselector("rLeg");
+var lLeg = document.queryselector("lLeg");
+
+var guess1 = document.getElementsByClassName("guess1");
+var guess2 = document.getElementsByClassName("guess2");
+var guess3 = document.querySelector("guess3");
+var guess4 = document.querySelector("guess4");
+var guess5 = document.querySelector("guess5");
+var guess6 = document.querySelector("guess6");
+
+// var failtCount = 
+
+// var words = ["savory", "sweet", "food", "yummy", "steak", "pizza"];
+
+// var pickWord = words[Math.floor(Math.random() * words.length)];
+
+// var answerArray = [];
+
+// for (var i=0; i< pickWord.length; i++) {
+//     answersArray[i] = "_";
+// }
+
+// var remainingLetters = word.length;
+
+// function gameLogic() {
+//     while (remainingLetters = 0) {
+//         return 
+// }
+
+var word = {'savory'};
+
+
+
+// html element references
+
+
+
+
+
+
+// create all event listeners
+
+
+
+
+
+// any additional functions to make it work
+
+
+
+function gameLogic () {
+    if (guess1 !== s) {
+        alert("try another letter");
+        // change text to show head
+    }
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
